ce23d4cdbcff19b41b13416da812d4d3dd93bf81
Rodolfo HiOk <rodolfo.okino@gmail.com>
2022-10-24 23:36:13 -0300

    add openapi

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { ChatModule } from './chat/chat.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(ChatModule);
+
+  const documentConfig = new DocumentBuilder()
+    .setTitle('Chat API')
+    .setDescription('Totvs Twitch Live Code!')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, documentConfig);
+  SwaggerModule.setup('swagger-ui', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
